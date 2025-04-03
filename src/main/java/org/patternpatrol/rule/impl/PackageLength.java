6ed056f27e746c34ac74fee4667d56bd2d9de533
Andrew Helsby <ajhelsby@hotmail.com>
2025-04-03 19:05:04 +0400

    feat(#1): Sort import error

--- src/main/java/org/patternpatrol/rule/impl/PackageLength.java
+++ src/main/java/org/patternpatrol/rule/impl/PackageLength.java
@@ -4,11 +4,9 @@ import org.patternpatrol.model.CheckResult;
 import org.patternpatrol.model.DirectoryRule;
 import org.patternpatrol.rule.DirectoryPatternRule;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public class PackageLength implements DirectoryPatternRule {
     @Override
     public CheckResult check(final DirectoryRule directoryRule, final String targetPath, final String fullPath) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 }
--- src/main/java/org/patternpatrol/rule/impl/PackageRegex.java
+++ src/main/java/org/patternpatrol/rule/impl/PackageRegex.java
@@ -1,8 +1,5 @@
 package org.patternpatrol.rule.impl;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import org.patternpatrol.helper.TextCheckHelper;
 import org.patternpatrol.model.CheckResult;
 import org.patternpatrol.model.DirectoryRule;
 import org.patternpatrol.rule.DirectoryPatternRule;
@@ -10,6 +7,6 @@ import org.patternpatrol.rule.DirectoryPatternRule;
 public class PackageRegex implements DirectoryPatternRule {
     @Override
     public CheckResult check(final DirectoryRule directoryRule, final String targetPath, final String fullPath) {
-        throw new NotImplementedException();
+        throw new UnsupportedOperationException();
     }
 }
