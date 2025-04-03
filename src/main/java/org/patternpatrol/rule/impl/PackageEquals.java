850b5218278d5b71fb41d998e93cf99e9ca53999
Andrew Helsby <ajhelsby@hotmail.com>
2025-04-03 19:23:53 +0400

    feat(#1): Fix deploy pipeline and versions add new package functions (#51)
    * feat(#1): Fix deploy pipeline and versions add new package functions
    
    * feat(#1): Sort import error

---
+++ src/main/java/org/patternpatrol/rule/impl/PackageEquals.java
@@ -0,0 +1,12 @@
+package org.patternpatrol.rule.impl;
+
+import org.patternpatrol.model.CheckResult;
+import org.patternpatrol.model.DirectoryRule;
+import org.patternpatrol.rule.DirectoryPatternRule;
+
+public class PackageEquals implements DirectoryPatternRule {
+    @Override
+    public CheckResult check(final DirectoryRule directoryRule, final String targetPath, final String fullPath) {
+        throw new UnsupportedOperationException();
+    }
+}
